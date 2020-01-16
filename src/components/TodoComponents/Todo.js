a1026eb1645186a5da93184a1bd6ff7a27c2c9bf
taty2010 <shydalys123@gmail.com>
2020-01-16 00:21:33 -0600

    added the initial class components and function components to display the hardcoded array

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const ToDo = props =>{
+    return(
+        <div>
+            <p>{props.item.task}</p>
+        </div>
+    )
+}
+
+export default ToDo
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,21 @@
+import React from 'react';
+import ToDo from './Todo';
+
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+const ToDoList = props =>{
+    return(
+        <div>
+            {props.toDo.map(todo =>(
+                <ToDo 
+                    key = {todo.id}
+                    item={todo}
+                />
+            ))}
+        </div>
+    )
+}
+
+export default ToDoList
